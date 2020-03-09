2248a160c34766c226137d32668880fe9dcc01fe
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-09 12:57:01 +1100

    added update function

--- config/orm.js
+++ config/orm.js
@@ -21,21 +21,19 @@ function objToSql(ob) {
 }
 
 var orm = {
-//select all
-  all: function(tableInput, cb) {
+  //select all
+  all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  
-};
 
-//insert
-  create: function(table, cols, vals, cb) {
+  //insert
+  create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -47,12 +45,35 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
+
   },
-  
+
+
+  //update
+  update: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+
+};
+
+
 module.exports = orm;
