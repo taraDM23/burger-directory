bcaa9668d5e7ab3d605eaafc70a946c50f597366
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-09 12:53:26 +1100

    added select all function

--- config/orm.js
+++ config/orm.js
@@ -21,8 +21,7 @@ function objToSql(ob) {
 }
 
 var orm = {
-
-  //select all
+//select all
   all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
@@ -33,44 +32,6 @@ var orm = {
     });
   },
   
-  //insert
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  
-  //update
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  }
 };
 
 module.exports = orm;
