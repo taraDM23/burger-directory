272bad3509bd14441794c1f13fed6b09f848720e
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-09 12:51:14 +1100

    included seed file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried ChickenBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BTL', FALSE);
