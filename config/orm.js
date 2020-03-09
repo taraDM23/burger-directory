b62426e6a93ae8163652c6b76e25d9e0f77e934f
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-09 12:54:09 +1100

    added insert new burger function

--- config/orm.js
+++ config/orm.js
@@ -34,4 +34,25 @@ var orm = {
   
 };
 
+//insert
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  
 module.exports = orm;
